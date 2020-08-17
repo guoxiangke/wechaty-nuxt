3bd085ba1688ca02a406fffdbfd944b7578ef6ad
dale@MBP <xiangkeguo@gmail.com>
2020-08-18 07:58:32 +0800

    fixed socket host

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import io from 'socket.io-client'
 import VueSocketIOExt from 'vue-socket.io-extended'
-
+import dotenv from 'dotenv'
+dotenv.config()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 const socket = io(`http://${host}:${port}`)
