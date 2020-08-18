116021987f44faf5b77ffb6f122e27927fdfbdaa
dale@MBP <xiangkeguo@gmail.com>
2020-08-18 10:51:03 +0800

    add @nuxtjs/proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,8 @@ module.exports = {
    ** Nuxt.js modules
    */
   modules: [
+    // Doc: https://github.com/nuxt-community/proxy-module
+    '@nuxtjs/proxy',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
